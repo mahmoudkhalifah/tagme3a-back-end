c1a1d14343bae3848f4e5e685c94512560ff684a
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2023-04-12 00:07:07 +0200

    init structre

---
+++ tagme3a-back-end.DAL/Data/Context/MainDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tagme3a_back_end.DAL.Data.Context
+{
+    public class MainDbContext : DbContext
+    {
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options)
+        {
+            
+        }
+
+
+    }
+}
